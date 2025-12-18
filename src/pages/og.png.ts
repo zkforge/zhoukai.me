bbe5011838e6e1853e9c874aa7911441114a0a03
zoukai <kaizhou0305@gmail.com>
2025-12-18 14:06:24 +0800

    bugfix : update problem

--- src/pages/og.png.ts
+++ src/pages/og.png.ts
@@ -1,7 +1,19 @@
+// import type { APIRoute } from "astro";
+// import { generateOgImageForSite } from "@/utils/generateOgImages";
+
+// export const GET: APIRoute = async () =>
+//   new Response(await generateOgImageForSite(), {
+//     headers: { "Content-Type": "image/png" },
+//   });
 import type { APIRoute } from "astro";
 import { generateOgImageForSite } from "@/utils/generateOgImages";
 
-export const GET: APIRoute = async () =>
-  new Response(await generateOgImageForSite(), {
-    headers: { "Content-Type": "image/png" },
+export const GET: APIRoute = async () => {
+  const buffer = await generateOgImageForSite();
+
+  return new Response(new Uint8Array(buffer), {
+    headers: {
+      "Content-Type": "image/png",
+    },
   });
+};
